05382e0c2c0f978494e75b08c7b1dc4395225f2b
Anthony Liang <liangtfm@gmail.com>
2024-06-15 18:16:52 -0700

    Fix import error

---
+++ pages/profile/[renterId]/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Flex, Text } from "@mantine/core";
+
+export default function Renter() {
+  return (
+    <Flex
+      direction={"column"}
+      pos={"relative"}
+      justify={"center"}
+      align={"center"}
+      h={"100%"}
+      w={"100%"}
+    >
+      <Text
+        variant="gradient"
+        fw={"800"}
+        size={"xl"}
+        gradient={{ from: "violet", to: "cyan", deg: 45 }}
+        style={{ fontSize: "3rem", textAlign: "center" }}
+        p={16}
+      >
+        Renter
+      </Text>
+    </Flex>
+  );
+}
